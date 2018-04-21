4ed6381588008cd3ff703b71ca2c4572e4c0884e
leaf corcoran <leafot@gmail.com>
2018-04-21 14:25:05 -0700

    add fullscreen distortion shader

--- FPS.gd
+++ FPS.gd
@@ -1,6 +1,6 @@
 extends Label
 
-onready var skate_timer = get_node("/root/Main/Sprites/Player/SkateSoundTimer")
+# onready var skate_timer = get_node("/root/Main/Sprites/Player/SkateSoundTimer")
 
 func _process(delta): 
-	text = "FPS: " + str(Engine.get_frames_per_second()) + ", Timer: " + str(skate_timer.wait_time)
+	text = "FPS: " + str(Engine.get_frames_per_second())
